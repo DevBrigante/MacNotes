103c0e8a34fe67ee8280095c811fc1c788e07251
Brenno Brigante <130163940+DevBrigante@users.noreply.github.com>
2026-09-02 23:47:44 -0300

    chore: add Xcode project scaffold and gitignore (#18)

---
+++ MacNotesTests/MacNotesTests.swift
@@ -0,0 +1,19 @@
+//
+//  MacNotesTests.swift
+//  MacNotesTests
+//
+//  Created by Brenno Reis on 02/09/26.
+//
+
+import Testing
+@testable import MacNotes
+
+struct MacNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
