29163b49ce54b632af72eafbf0f56768effed1e7
Brenno <brennoreis411@gmail.com>
2026-09-02 23:40:28 -0300

    chore: add Xcode project scaffold and gitignore

---
+++ MacNotes/MacNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacNotesApp.swift
+//  MacNotes
+//
+//  Created by Brenno Reis on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MacNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
